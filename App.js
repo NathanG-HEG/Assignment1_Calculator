cc320b19441156cbb9e59d1843ecb9fc193bac23
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2022-08-30 11:37:08 +0300

    Simple calculator

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [result, setResult] = useState('Result:');
+    const [a, setA] = useState('');
+    const [b, setB] = useState('');
+
+    const addition = () => {
+        let res = parseFloat(a) + parseFloat(b);
+        setResult('Result: ' + res);
+    }
+
+    const substraction = () => {
+        let res = parseFloat(a) - parseFloat(b);
+        setResult('Result: ' + res);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text
+                style={styles.result}>
+                {result}
+            </Text>
+            <TextInput
+                keyboardType="numeric"
+                style={styles.textInput}
+                onChangeText={a => setA(a)} value={a}/>
+            <TextInput
+                keyboardType="numeric"
+                style={styles.textInput}
+                onChangeText={b => setB(b)} value={b}/>
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                <Pressable
+                    style={styles.button}
+                    onPress={addition}>
+                    <Text style={styles.text}>+</Text>
+                </Pressable>
+                <Pressable
+                    style={styles.button}
+                    onPress={substraction}>
+                    <Text style={styles.text}>-</Text>
+                </Pressable>
+            </View>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textInput: {
+        borderColor: "#000",
+        height: 44,
+        marginTop: 11,
+        marginBottom: 5,
+        width: 200,
+        borderWidth: 1
+    },
+    result: {
+        fontSize: 16
+    },
+    button: {
+        paddingVertical: 11,
+        paddingHorizontal: 11,
+        marginTop: 11,
+        backgroundColor: 'lightblue',
+        marginHorizontal: 11
+    }
 });
